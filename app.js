9f53bdc11594f80f0b0a9cd59d4001059783d025
Yasir-Mohamud <ymohamed018@gmail.com>
2020-02-06 13:45:59 -0800

    updates js with function

--- app.js
+++ app.js
@@ -1,73 +1,38 @@
-/* eslint-disable no-unreachable */
-/* eslint-disable eqeqeq */
+
 'use strict';
 var score = 0;
 var user = prompt('What is your name?');
 alert('Welcome ' + user);
 
 // question 1
-var age = prompt('Is the sky blue ?').toLowerCase();
-if (age === 'yes') {
-  //console.log('You are old enough to LEGALLY drink!');
-  alert('correct');
-  score++;
-} else if (age === 'no') {
-  //console.log('You are still to young :(');
-  alert('wrong');
-}
+var question = prompt('Is the sky blue ?');
+ques();
 
 //question 2
-var days = prompt('Are there 7 days in a week').toLowerCase();
-if (days === 'yes') {
-  //console.log('Congrats on finishing high school!');
-  alert('correct');
-  score++;
-} else if (days === 'no') {
-  //console.log('You did not gradute');
-  alert('wrong');
-}
+var question = prompt('Are there 7 days in a week');
+ques();
 
 //question 3
-var anime = prompt('Is Anime fun?');
-if (anime.toLowerCase() === 'yes') {
-  //console.log('I watch Anime too!');
-  alert('correct');
-  score++;
-} else if (anime.toLowerCase() === 'no') {
-  //console.log('You should start watching it');
-  alert('wrong');
-}
+var question = prompt('Is Anime fun?');
+ques();
 
 //questiion 4
-var sports = prompt('is basketball a sport');
-if (sports.toLowerCase() === 'yes') {
-  // console.log('I love to watch or play sports as well');
-  alert('correct');
-  score++;
-} else if (sports.toLowerCase() === 'no') {
-  //console.log('great to know');
-  alert('wrong');
-}
+var question = prompt('is basketball a sport');
+ques();
+
 //question 5
-var coding = prompt('Is the first week of coding hard for you?');
-if (coding.toLowerCase() === 'yes') {
-  // console.log('Everything in life starts off hard');
-  alert('correct');
-  score++;
-} else if (coding.toLowerCase() === 'no') {
-  // console.log('Well the second is harder : ) ');
-  alert('wrong');
-}
+var question = prompt('Is the first week of coding hard for you?');
+ques();
 
 //question 6
 for (var i = 0; i < 4; i++ ) {
   var correct = 7;
   var num = prompt('Can you guess a number between 1 and 10');
   if ( num == correct) {
-    alert('GOOD JOB! Youre answer is correct');
-    break;
-    // eslint-disable-next-line no-unreachable
+    alert('GOOD JOB!');
     score++;
+    break;
+
   } else if (num < correct) {
     alert('Too low ');
   } else if (num > correct) {
@@ -86,25 +51,24 @@ var isCorrect = false;
 for (var j = 0 ; j < 6; j++){
   var edu =  prompt(' Name a computer software language ');
   var lang = ['javascript' , 'python', 'asp.net', 'java'];
-  // eslint-disable-next-line no-redeclare
+
   for (var i = 0; i < lang.length ; i++){
     if (edu.toLowerCase() == lang[i]) {
       alert('You are correct!');
       isCorrect = true;
-      break;
-      // eslint-disable-next-line no-unreachable
       score++;
+      break;
     }
 
   }
   break;
-
   // eslint-disable-next-line eqeqeq
   if (isCorrect == false){
     alert('try again');
   }
 
 }
+// eslint-disable-next-line eqeqeq
 if (isCorrect == false > 6){
   alert('answers are ' + lang);
 }
@@ -112,3 +76,12 @@ if (isCorrect == false > 6){
 alert('Your score is '+ score);
 
 
+
+function ques() {
+  if (question.toLowerCase() === 'yes') {
+    alert('correct');
+    score++;
+  } else {
+    alert('wrong');
+  }
+}
