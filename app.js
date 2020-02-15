06f7a93dde3eb0ae43de5103ed70d5433615ee2c
Yasir-Mohamud <ymohamed018@gmail.com>
2020-02-15 15:57:24 -0800

    fixes question 7

--- app.js
+++ app.js
@@ -1,57 +1,59 @@
 
-'use strict';
-var score = 0;
-var user = prompt('What is your name?');
-alert('Welcome ' + user);
+// 'use strict';
+// var score = 0;
+// var user = prompt('What is your name?');
+// alert('Welcome ' + user);
 
-// question 1
-var question = prompt('Is the sky blue ?');
-ques();
+// // question 1
+// var question = prompt('Is the sky blue ?');
+// ques();
 
-//question 2
-var question = prompt('Are there 7 days in a week');
-ques();
+// //question 2
+// var question = prompt('Are there 7 days in a week');
+// ques();
 
-//question 3
-var question = prompt('Is Anime fun?');
-ques();
+// //question 3
+// var question = prompt('Is Anime fun?');
+// ques();
 
-//questiion 4
-var question = prompt('is basketball a sport');
-ques();
+// //questiion 4
+// var question = prompt('is basketball a sport');
+// ques();
 
-//question 5
-var question = prompt('Is the first week of coding hard for you?');
-ques();
+// //question 5
+// var question = prompt('Is the first week of coding hard for you?');
+// ques();
 
-//question 6
-for (var i = 0; i < 4; i++ ) {
-  var correct = 7;
-  var num = prompt('Can you guess a number between 1 and 10');
-  if ( num == correct) {
-    alert('GOOD JOB!');
-    score++;
-    break;
+// //question 6
+// for (var i = 0; i < 4; i++ ) {
+//   var correct = 7;
+//   var num = prompt('Can you guess a number between 1 and 10');
+//   if ( num == correct) {
+//     alert('GOOD JOB!');
+//     score++;
+//     break;
 
-  } else if (num < correct) {
-    alert('Too low ');
-  } else if (num > correct) {
-    alert('Too high');
-  }
-
-
-}
+//   } else if (num < correct) {
+//     alert('Too low ');
+//   } else if (num > correct) {
+//     alert('Too high');
+//   }
 
 
-alert('The correct answer is ' + correct);
+// }
 
-//question 7
+
+// alert('The correct answer is ' + correct);
+
+// question 7
+
+
+var lang = ['javascript' , 'python' , 'asp.net' , 'java' ];
+
 var isCorrect = false;
 
 for (var j = 0 ; j < 6; j++){
   var edu =  prompt(' Name a computer software language ');
-  var lang = ['javascript' , 'python', 'asp.net', 'java'];
-
   for (var i = 0; i < lang.length ; i++){
     if (edu.toLowerCase() == lang[i]) {
       alert('You are correct!');
@@ -61,27 +63,21 @@ for (var j = 0 ; j < 6; j++){
     }
 
   }
-  break;
-  // eslint-disable-next-line eqeqeq
-  if (isCorrect == false){
+
+  if (!isCorrect){
     alert('try again');
   }
 
 }
-// eslint-disable-next-line eqeqeq
-if (isCorrect == false > 6){
-  alert('answers are ' + lang);
-}
+alert('Answers are ' + lang);
 
-alert('Your score is '+ score);
+// alert('Your score is ' + score);
 
-
-
-function ques() {
-  if (question.toLowerCase() === 'yes') {
-    alert('correct');
-    score++;
-  } else {
-    alert('wrong');
-  }
-}
+// function ques() {
+//   if (question.toLowerCase() === 'yes') {
+//     alert('correct');
+//     score++;
+//   } else {
+//     alert('wrong');
+//   }
+// }
